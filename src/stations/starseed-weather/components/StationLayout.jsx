2f48f207d1e8bbe4680aca4c7a47127c8a475f69
daphneblum <152042198+daphneblum@users.noreply.github.com>
2026-07-11 22:31:58 -0500

    Build Layout

---
+++ src/stations/starseed-weather/components/StationLayout.jsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router-dom";
+import '../starseed.css'
+
+function StationLayout({ children }) {
+    return (
+        <div className="starseed-station">
+            <nav className="starseed-nav">
+                <h1 className="starseed-nav__title">
+                    Starseed<br />Weather
+                </h1>
+                <NavLink
+                    to="/"
+                    end
+                    className={({ isActive }) => 
+                        `starseed-nav__link${isActive ? ' starseed-nav__link--active' : ''}`
+                    }
+                >
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="/forecast"
+                    className={({ isActive }) =>
+                        `starseed-nav__link${isActive ? ' starseed-nav__link--active' : ''}`
+                    }    
+                >
+                    Forecast
+                </NavLink>
+            </nav>
+
+            <div className="starseed-main">
+                <div className="starseed-content">
+                    {children}
+                </div>
+                {/* TODO: <BroadcastTicker /> */}
+            </div>
+        </div>
+    )
+}
+
+export default StationLayout;
